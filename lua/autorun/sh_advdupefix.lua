e1480d2dac9e02cfd95f3582748cb18930ee159d
TheMerkyShadow <32728280+TheMerkyShadow@users.noreply.github.com>
2018-01-25 03:57:18 -0500

    Update sh_advdupefix.lua

--- lua/autorun/sh_advdupefix.lua
+++ lua/autorun/sh_advdupefix.lua
@@ -1,1 +1,1 @@
-table.foreach( scripted_ents.GetList(), function(ent) if ent.t.Author == "Syncaidius" then ent.t.Spawnable = true end end)
+table.foreach( scripted_ents.GetList(), function(val,ent) if ent.t.Author == "Syncaidius" then ent.t.Spawnable = true end end)
