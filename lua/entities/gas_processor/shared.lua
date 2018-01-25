d58200e431e7bacd5df40128dbc0f165bbfefab8
TheMerkyShadow <32728280+TheMerkyShadow@users.noreply.github.com>
2018-01-25 00:36:59 -0500

    Update shared.lua

--- lua/entities/gas_processor/shared.lua
+++ lua/entities/gas_processor/shared.lua
@@ -3,7 +3,7 @@ ENT.Base 		= "base_rd3_entity"
 
 list.Set( "LSEntOverlayText" , "gas_processor", {HasOOO = true, num = 2, resnames = {"energy","Natural Gas"}} )
 
-ENT.PrintName	    = "Natural Gas Processor"
+ENT.PrintName	    = "Success"
 ENT.Author		    = "Syncaidius"
 ENT.Instructions    = ""
 ENT.Contact		    = "syncaidius@googlemail.com"
