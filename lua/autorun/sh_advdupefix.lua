93376470dbc1d4cb04274ea19fafa8bed03d6293
TheMerkyShadow <32728280+TheMerkyShadow@users.noreply.github.com>
2018-01-25 20:46:22 -0500

    Update sh_advdupefix.lua

--- lua/autorun/sh_advdupefix.lua
+++ lua/autorun/sh_advdupefix.lua
@@ -1,1 +1,3 @@
-table.foreach( scripted_ents.GetList(), function(val,ent) if ent.t.Author == "Syncaidius" then ent.t.Spawnable = true end end)
+hook.Add( "InitPostEntity", "adv_dupe_fix", function()
+	table.foreach( scripted_ents.GetList(), function(val,ent) if ent.t.Author == "Syncaidius" then ent.t.Spawnable = true end end)
+end)
